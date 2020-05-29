cc63d14d88a2c5f839d031cf3e4d6696dbd345ee
elenaslavutina <elena.slavutina2013@gmail.com>
2020-05-29 17:55:33 +0300

    some changes in tests

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+    
     @Test
     void shouldReturnBonusIfAmountMoreThan1000() {
-        CashbackHackService service = new CashbackHackService();
+
         int amount =1100;
 
         int actual = service.calculateBonus(amount);
@@ -17,7 +19,6 @@ class CashbackHackServiceTest {
     }
 @Test
     void shouldReturnMaxBonusIfAmountMoreThanMaximum() {
-        CashbackHackService service = new CashbackHackService();
         int amount =10_500;
 
         int actual = service.calculateBonus(amount);
@@ -28,7 +29,6 @@ class CashbackHackServiceTest {
 
     @Test
     void shouldReturnZeroIfAmountLowerThan1000() {
-        CashbackHackService service = new CashbackHackService();
         int amount = 600;
 
         int actual = service.calculateBonus(amount);
@@ -39,7 +39,6 @@ class CashbackHackServiceTest {
 
     @Test
     void shouldOfferBuySomethingForMaxBonus() {
-        CashbackHackService service = new CashbackHackService();
         int amount = 1900;
 
         int actual = service.calculateBonus(amount);
