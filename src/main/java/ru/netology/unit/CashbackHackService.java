aea5f98e3aea10705d9c0af599c5143cc299b622
elenaslavutina <elena.slavutina2013@gmail.com>
2020-06-03 13:01:54 +0300

    fixed 5

--- src/main/java/ru/netology/unit/CashbackHackService.java
+++ src/main/java/ru/netology/unit/CashbackHackService.java
@@ -2,13 +2,10 @@ package ru.netology.unit;
 
 public class CashbackHackService {
     private final int boundary = 1000;//минимальная нижняя граница начисления кэшбэка
+        public int remain(int amount) {
+            return boundary - amount % boundary;
+        }
 
-    public int remain(int amount) {
-        if (amount%boundary == 0) return 0;
-        if ((boundary - amount % boundary) == 100)
-            System.out.println("Suggest you buy something to 100rub for max bonus");
-        return boundary - amount % boundary;
-    }
 
     public int getBoundary() {
         return boundary;
